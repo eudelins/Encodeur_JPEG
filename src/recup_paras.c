c07a93d080cbb070f58e2618462afc3423719952
boulhole <elodie.boulhol@grenoble-inp.org>
2020-05-14 19:33:48 +0200

    maj

--- src/recup_paras.c
+++ src/recup_paras.c
@@ -27,36 +27,63 @@ void free_pointeur(char **pointeur) {
 }
 
 
-/* Récupère les paramètes optionnels */
-char **paras_optionnels(uint8_t argc, char **argv) {
-    if (argc <= 2) {
-        char **paras = malloc(12*sizeof(char));
-        paras[0] = "pas de paras";
-        return paras;
-    }
-
-    uint16_t longueur = 0;
-    for (uint8_t i = 2; i < argc; i++) {
-        longueur += strlen(argv[i]);
-    }
-    char **paras = malloc(longueur * sizeof(char));
-
-    for(uint8_t i = 2; i < argc; i++) {
-        paras[i-2] = argv[i];
-    }
-
-    printf("%s, %s\n", paras[0], paras[1]);
-
-    return paras;
+/* Récupère une chaine de caractères de type --chaine et renvoie le premier caractère de chaine*/
+char premier_caractr(char *chaine) {
+    char prem;
+    sscanf(chaine, "--%c", prem);
+    return prem;
 }
 
 
-int main(uint16_t argc, char **argv) {
-    char *chemin = chemin_fichier(argv[1]);
-    printf("%s\n", chemin);
-    free(chemin);
 
-    char **parametres = paras_optionnels(argc, argv);
-    free_pointeur(parametres);
+///* Récupère les paramètes optionnels */
+//char *paras_optionnels(uint8_t argc, char **argv) {
+//    if (argc <= 2) {
+//        char *paras = malloc(sizeof(char));
+//        paras = "r";
+//        return paras;
+//    }
+//
+//    bool help = false;
+//    bool outfile = false;
+//    bool sample = false;
+//    uint16_t longueur = 0;
+//    for (uint8_t i = 2; i < argc; i++) {
+//        if (argv[i] == "--help") {
+//            help = true;
+//        }
+//        elif (argv[i] == "--"
+//        longueur += strlen(argv[i]);
+//    }
+//
+//    /* Si on a au moins un --help -> prime sur les autres */
+//    if (help == true) {
+//        char *paras = malloc(sizeof(char));
+//        paras = "h";
+//    }
+//    else {
+//        char **paras = malloc(longueur * sizeof(char));
+//    }
+//
+//    for(uint8_t i = 2; i < argc; i++) {
+//        paras[i-2] = argv[i];
+//    }
+//
+//
+//    return paras;
+//}
+//
+
+int main(uint16_t argc, char **argv) {
+
+    char prem = premier_caractr(argv[1]);
+    printf("%c\n", prem);
+
+    //char *chemin = chemin_fichier(argv[1]);
+    //printf("%s\n", chemin);
+    //free(chemin);
+
+    //char **parametres = paras_optionnels(argc, argv);
+    //free_pointeur(parametres);
     return 0;
 }
