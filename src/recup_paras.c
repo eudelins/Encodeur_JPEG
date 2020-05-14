0e71855babd5a3e7c1cf3138d02eeac8af3b3f8f
boulhole <elodie.boulhol@grenoble-inp.org>
2020-05-14 16:12:42 +0200

    maj

--- src/recup_paras.c
+++ src/recup_paras.c
@@ -6,6 +6,12 @@
 
 /* Récupère le nom de l'image à encoder en ligne de commande et renvoie le chemin d'accès en chaine de caractères */
 char *chemin_fichier(char *nom_fichier) {
+    if (nom_fichier == NULL) {
+        char *chemin = malloc(14 * sizeof(char));
+        chemin = "pas de fichier";
+        return chemin;
+    }
+
     char *chemin =  malloc((11 + strlen(nom_fichier)) * sizeof(char));
     strcpy(chemin, "../images/");
     strcat(chemin, nom_fichier);
