6c42d944b3694a142c26b8b1b14ba0a1633efd5e
boulhole <elodie.boulhol@grenoble-inp.org>
2020-05-20 11:13:43 +0200

    ajout du sous-échantillonage des compo Cb Cr. Manque encore des sous-fonctions. En attente du decoupe_tout_couleur.c pour tester

--- src/sous_echantillonnage.c
+++ src/sous_echantillonnage.c
@@ -118,18 +118,60 @@ bool verif_conditions(uint32_t h1,
 
 
 /* Partitionne l'image en MCUs de taille h1 x v1 */
+// utilise un module de Val
 
 
+/* Sous échantillonnage d'une MCU de taille h1 x v1 à sous-échantillonner en une taille donnée en argument : h x v */
+struct MCU ***sous_echantillonnage_MCU(uint8_t h1,
+                                       uint8_t v1,
+                                       uint8_t h,
+                                       uint8_t v,
+                                       struct MCU ***MCU_a_sous_echantillonner)
+{
+    MCU_a_sous_echantillonner -> largeur = h;
+    MCU_a_sous_echantillonner -> longueur -> = v;
+
+    uint8_t taux_largeur = h1/h;
+    uint8_t taux_longueur = v1/v;
+    struct pixel pixel_prec;
+
+    // une matrice de pixels qui récupère tous les pixels d'une MCU de taille h1 x v1
+    struct pixel ***matrice_pixels = matrice_pixels_MCU(h1, v1, MCU_a_sous_echantillonner);
+
+    // on parcourt le nb_largeur nécessaire qui vaut 8 * taux_largeur et nb_longueur nécessaire qui vaut 8 * taux_longueur dans matrice_pixels
+    for (largeur_MCU = 0; largeur_MCU < 8 * taux_largeur; largeur_MCU++) {          // revoir les noms de paramètres ?
+        for (hauteur_MCU = 0; hauteur_MCU < 8 * taux_longueur; hauteur_MCU++) {     // idem
+            for (hauteur_pix = 0; hauteur_pix < 8; hauteur_pix++) {
+                struct pixel *ligne_pixels = malloc(); // A REMPLIR
+                for (largeur_pix = 0; largeur_pix < 8; largeur_pix++) {
+                    if (largeur_pix % 2 == 0) {
+                        struct pixel nouveau_pixel;
+                        // on fait la moyenne des 2 pixels voisins
+                        nouveau_pixel = moyenne(pixel_prec, matrice_pixels[largeur_pix][hauteur_pix]);
+                        // on met ce nouveau pixel à la place largeur_pix / 2 dans la MCU
+                        MCU_a_sous_echantillonner->pixels[largeur_pix / 2][hauteur_pix] = nouveau_pixel;
+                        // on met aussi le pixel à la place largeur dans le bloc
+                        MCU_a_sous_echantillonner->bloc->pixel[largeur_pix][hauteur_pix] = nouveau_pixel;
+                    }
+                    else {
+                        pixel_prec = MCU_a_sous_echantillonner->pixels[largeur_pix][hauteur_pix];   // sinon, on avance d'un pixel
+                    }
+                }
+            }
+        }
+    }
 
 int main()
 {
-    uint8_t h1 = 4;
+    uint8_t h1 = 2;
     uint8_t v1 = 2;
     uint8_t h2 = 1;
     uint8_t v2 = 2;
     uint8_t h3 = 1;
-    uint8_t v3 = 1;
+    uint8_t v3 = 2;
 
     bool conditions = verif_conditions(h1, v1, h2, v2, h3, v3);
+    // faire la partition en MCUs de taille h1xv1
+    // pour chaque MCU : sous-échantillonner les compmosantes Cb et Cr
     return 0;
 }
