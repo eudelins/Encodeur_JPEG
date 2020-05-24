35c2f68d9019cc6b294ca3503e67b3be67616159
boulhole <elodie.boulhol@grenoble-inp.org>
2020-05-24 10:41:50 +0200

    modif syntaxe

--- include/decoupe_tout_couleur.h
+++ include/decoupe_tout_couleur.h
@@ -18,6 +18,29 @@ FILE *ouvrir_fichier(const char *filename,
 void fermer_fichier(FILE *fichier);
 
 
+/*Défini un pixel en RGB*/
+struct Pixel_RGB {
+  uint8_t R;
+  uint8_t G;
+  uint8_t B;
+};
+
+
+/* Défini un bloc */
+struct Bloc_RGB {
+    struct Pixel_RGB **pixels;
+};
+
+
+/* Défini une MCU */
+struct MCU_RGB {
+    uint8_t largeur;
+    uint8_t hauteur;
+    struct Pixel_RGB **pixels;
+    struct Bloc_RGB **blocs;
+};
+
+
 /* Calcule le nombre de MCUs dans l'image */
 uint32_t *calcul_dimensions_MCUs(uint32_t largeur_image,
                                  uint32_t hauteur_image,
@@ -31,7 +54,7 @@ void free_pixel_image(struct Pixel_RGB **pixels,
 
 
 /* Découpe l'image en MCUs : nb_MCUs_largeur x nb_MCUs_hauteur */
-struct MCU ***decoupage_MCUs(FILE *fichier,
+struct MCU_RGB ***decoupage_MCUs(FILE *fichier,
                              uint32_t largeur_image,
                              uint32_t hauteur_image,
                              uint32_t nb_MCUs_largeur,
@@ -41,13 +64,13 @@ struct MCU ***decoupage_MCUs(FILE *fichier,
 
 
 /* Découpe une MCU en blocs pour une MCU de taille largeur_MCU x hauteur_MCU */
-void decoupage_blocs(struct MCU *MCU,
+void decoupage_blocs(struct MCU_RGB *MCU,
                      uint8_t largeur_MCU,
                      uint8_t hauteur_MCU);
 
 
 /* Découpe toutes les MCU en blocs de tailles largeur_MCU x hauteur_MCU */
-struct MCU ***decoupage_MCUs_en_blocs(struct MCU ***MCUs,
+struct MCU_RGB ***decoupage_MCUs_en_blocs(struct MCU_RGB ***MCUs,
                                       uint32_t nb_MCUs_largeur,
                                       uint32_t nb_MCUs_hauteur,
                                       uint8_t largeur_MCU,
@@ -59,13 +82,13 @@ void free_pixel(struct Pixel_RGB **pixels);
 
 
 /* Libère la mémoire allouée aux bloc */
-void free_blocs(struct Bloc **blocs,
+void free_blocs(struct Bloc_RGB **blocs,
                 uint8_t largeur_MCU,
                 uint8_t hauteur_MCU);
 
 
 /* Libère la mémoire allouée aux MCUs */
-void free_MCUs_dims(struct MCU ***MCUs,
+void free_MCUs_dims(struct MCU_RGB ***MCUs,
                     uint32_t* dimensions_MCUs,
                     uint8_t largeur_MCU,
                     uint8_t hauteur_MCU);
@@ -76,19 +99,19 @@ void print_pixel(struct Pixel_RGB pixel);
 
 
 /* Affiche un bloc */
-void print_bloc(struct Bloc bloc);
+void print_bloc(struct Bloc_RGB bloc);
 
 
 /* Affiche une MCU */
-void print_MCU(struct MCU *MCU,
+void print_MCU(struct MCU_RGB *MCU,
                uint8_t largeur_MCU,
                uint8_t hauteur_MCU);
 
 
 /* Affiche les MCUs */
-void print_MCUs(struct MCU ***MCUs,
+void print_MCUs(struct MCU_RGB ***MCUs,
                 uint32_t *dimensions_MCUs,
                 uint8_t largeur_MCU,
                 uint8_t hauteur_MCU);
 
-
+#endif /* DECOUPE_TOUT_COULEUR_H */
