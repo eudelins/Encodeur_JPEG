747fa40bdea193a8f48c3ea5b10e1f7f15919e2d
boulhole <elodie.boulhol@grenoble-inp.org>
2020-05-25 20:04:14 +0200

    petite modif

--- src/decoupe_tout_couleur.c
+++ src/decoupe_tout_couleur.c
@@ -217,14 +217,14 @@ void free_blocs_RGB(struct Bloc_RGB **blocs,
 void free_MCUs_dims_RGB(struct MCU_RGB ***MCUs,
                     uint32_t* dimensions_MCUs)
 {
-    uint8_t h1 = MCUs[0][0]->h1;
-    uint8_t v1 = MCUs[0][0]->v1;
-
     uint32_t nb_MCUs_hauteur, nb_MCUs_largeur;
     nb_MCUs_largeur = dimensions_MCUs[0];
     nb_MCUs_hauteur = dimensions_MCUs[1];
     for (int32_t hauteur = nb_MCUs_hauteur - 1; hauteur >= 0; hauteur--){
         for (int32_t largeur = nb_MCUs_largeur - 1; largeur >= 0; largeur--){
+            uint8_t h1 = MCUs[hauteur][largeur]->h1;   // changement ici
+            uint8_t v1 = MCUs[hauteur][largeur]->v1;
+
             free_pixel_RGB(MCUs[hauteur][largeur]->pixels);
             free_blocs_RGB(MCUs[hauteur][largeur]->blocs, h1, v1);
             free(MCUs[hauteur][largeur]);
@@ -290,7 +290,7 @@ void print_MCUs_RGB(struct MCU_RGB ***MCUs,
 
 // int main(void)
 // {
-//     FILE *fichier = ouvrir_fichier("images/shaun_the_sheep.ppm", "r");
+//     FILE *fichier = fopen("../images/shaun_the_sheep.ppm", "r");
 //
 //     // On récupère l'en-tête (P5 ou P6)
 //     char en_tete[10];
@@ -303,8 +303,8 @@ void print_MCUs_RGB(struct MCU_RGB ***MCUs,
 //     sscanf(dimensions, "%u %u", &largeur_image, &hauteur_image);
 //
 //     // On calcule les dimensions des MCUs
-//     uint8_t h1 = 1;
-//     uint8_t v1 = 1;
+//     uint8_t h1 = 2;
+//     uint8_t v1 = 2;
 //     uint8_t h2 = 1;
 //     uint8_t v2 = 1;
 //     uint8_t h3 = 1;
@@ -317,8 +317,8 @@ void print_MCUs_RGB(struct MCU_RGB ***MCUs,
 //
 //     struct MCU_RGB ***MCUs = decoupage_MCUs(fichier, largeur_image, hauteur_image, nb_MCUs_largeur, nb_MCUs_hauteur, h1, v1, h2, v2, h3, v3);
 //     MCUs = decoupage_MCUs_en_blocs(MCUs, nb_MCUs_largeur, nb_MCUs_hauteur, h1, v1);
-//     print_MCUs_RGB(MCUs, dimensions_MCUs, h1, v1);
+//     print_MCUs_RGB(MCUs, dimensions_MCUs);
 //     free_MCUs_dims_RGB(MCUs, dimensions_MCUs);
-//     fermer_fichier(fichier);
+//     fclose(fichier);
 //     return 0;
 // }
