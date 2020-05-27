bcbc69eadffd9188485f11e3bf6c6e4a3b70f3f1
boulhole <elodie.boulhol@grenoble-inp.org>
2020-05-27 12:02:14 +0200

    à jour

--- src/decoupe_couleur.c
+++ src/decoupe_couleur.c
@@ -90,17 +90,20 @@ struct MCU_RGB ***decoupage_MCUs(FILE *fichier,
     struct Pixel_RGB **pixels_image = malloc((hauteur_image + duplique_ligne) * sizeof(struct Pixel_RGB *));
     for (uint32_t hauteur = 0; hauteur < hauteur_image; hauteur++){
         struct Pixel_RGB *ligne_pixels_image = malloc((largeur_image + duplique_colonne) * sizeof(struct Pixel_RGB*));
+
         // On parcourt l'image avec fgetc et on rempli ligne_pixels_image pour ensuite remplir pixels_image
         for (uint32_t largeur = 0; largeur < largeur_image; largeur++){
           ligne_pixels_image[largeur].R = fgetc(fichier);
           ligne_pixels_image[largeur].G = fgetc(fichier);
           ligne_pixels_image[largeur].B = fgetc(fichier);
         }
+        
         for (uint32_t largeur = largeur_image; largeur < largeur_image + duplique_colonne; largeur++){
           ligne_pixels_image[largeur] = ligne_pixels_image[largeur_image - 1];
         }
         pixels_image[hauteur] = ligne_pixels_image;
     }
+    
     for (uint32_t hauteur = hauteur_image; hauteur < hauteur_image + duplique_ligne; hauteur++){
       pixels_image[hauteur] = pixels_image[hauteur_image - 1];
     }
@@ -110,6 +113,7 @@ struct MCU_RGB ***decoupage_MCUs(FILE *fichier,
     for (uint32_t hauteur = 0; hauteur < nb_MCUs_hauteur; hauteur++){
         // matrice de MCU
         struct MCU_RGB **ligne_MCUs = malloc(nb_MCUs_largeur * sizeof(struct MCU_RGB));
+        
         for (uint32_t largeur = 0; largeur < nb_MCUs_largeur; largeur++){
             // tableau de MCU
             struct MCU_RGB *MCU = malloc(sizeof(struct MCU_RGB));
@@ -126,6 +130,7 @@ struct MCU_RGB ***decoupage_MCUs(FILE *fichier,
 
             for (uint8_t hauteur_pix = 0; hauteur_pix < v1 * COTE_BLOC; hauteur_pix++){
                 struct Pixel_RGB *ligne_pixels = malloc(h1 * COTE_BLOC * sizeof(struct Pixel_RGB));
+            
                 for (uint8_t largeur_pix = 0; largeur_pix < h1 * COTE_BLOC; largeur_pix++){
                     ligne_pixels[largeur_pix] = pixels_image[hauteur * COTE_BLOC * v1 + hauteur_pix][largeur * COTE_BLOC * h1 + largeur_pix];
                 }
@@ -154,11 +159,14 @@ void decoupage_blocs(struct MCU_RGB *MCU)
     struct Bloc_RGB **blocs = malloc(v1 * sizeof(struct Bloc_RGB*));
     for (uint32_t hauteur = 0; hauteur < v1; hauteur++){
         struct Bloc_RGB *ligne_blocs = malloc(h1 * sizeof(struct Bloc_RGB));
+        
         for (uint32_t largeur = 0; largeur < h1; largeur++){
             // On recopie les pixels
             struct Pixel_RGB **pixels = malloc(COTE_BLOC * sizeof(struct Pixel_RGB *));
+            
             for (uint8_t hauteur_pix = 0; hauteur_pix < COTE_BLOC; hauteur_pix++){
                 struct Pixel_RGB *ligne_pixels = malloc(COTE_BLOC * sizeof(struct Pixel_RGB));
+                
                 for (uint8_t largeur_pix = 0; largeur_pix < COTE_BLOC; largeur_pix++){
                     ligne_pixels[largeur_pix] = MCU->pixels[hauteur * COTE_BLOC + hauteur_pix][largeur * COTE_BLOC + largeur_pix];
                 }
@@ -178,6 +186,7 @@ struct MCU_RGB ***decoupage_MCUs_en_blocs(struct MCU_RGB ***MCUs,
                                           uint32_t nb_MCUs_hauteur)
 {
     for (uint32_t hauteur = 0; hauteur < nb_MCUs_hauteur; hauteur++) {
+        
         for (uint32_t largeur = 0; largeur < nb_MCUs_largeur; largeur++) {
             decoupage_blocs(MCUs[hauteur][largeur]);
         }
@@ -203,6 +212,7 @@ void free_blocs_RGB(struct Bloc_RGB **blocs,
                     uint8_t v1)
 {
     for (int32_t hauteur = v1 - 1; hauteur >= 0; hauteur--){
+        
         for (int32_t largeur = h1 - 1; largeur >= 0; largeur--){
             free_pixel_RGB(blocs[hauteur][largeur].pixels, 1);
         }
@@ -220,6 +230,7 @@ void free_MCUs_dims_RGB(struct MCU_RGB ***MCUs,
     nb_MCUs_largeur = dimensions_MCUs[0];
     nb_MCUs_hauteur = dimensions_MCUs[1];
     for (int32_t hauteur = nb_MCUs_hauteur - 1; hauteur >= 0; hauteur--){
+        
         for (int32_t largeur = nb_MCUs_largeur - 1; largeur >= 0; largeur--){
             uint8_t h1 = MCUs[hauteur][largeur]->h1;
             uint8_t v1 = MCUs[hauteur][largeur]->v1;
@@ -246,6 +257,7 @@ void print_bloc_RGB(struct Bloc_RGB bloc)
 {
     struct Pixel_RGB **pixels = bloc.pixels;
     for (uint8_t hauteur_pix = 0; hauteur_pix < COTE_BLOC; hauteur_pix++){
+        
         for (uint8_t largeur_pix = 0; largeur_pix < COTE_BLOC; largeur_pix++){
             print_pixel_RGB(pixels[hauteur_pix][largeur_pix]);
         }
@@ -262,6 +274,7 @@ void print_MCU_RGB(struct MCU_RGB *MCU)
 
     struct Bloc_RGB **blocs = MCU->blocs;
     for (uint8_t hauteur_blocs = 0; hauteur_blocs < v1; hauteur_blocs++){
+        
         for (uint8_t largeur_blocs = 0; largeur_blocs < h1; largeur_blocs++){
             printf("Bloc_RGB %d:\n", hauteur_blocs * h1 + largeur_blocs);
             print_bloc_RGB(blocs[hauteur_blocs][largeur_blocs]);
@@ -279,6 +292,7 @@ void print_MCUs_RGB(struct MCU_RGB ***MCUs,
     nb_MCUs_largeur = dimensions_MCUs[0];
     nb_MCUs_hauteur = dimensions_MCUs[1];
     for (uint32_t hauteur = 0; hauteur < nb_MCUs_hauteur ; hauteur++){
+        
         for (uint32_t largeur = 0; largeur < nb_MCUs_largeur; largeur++){
             printf("MCU %d:\n", hauteur * nb_MCUs_largeur + largeur);
             print_MCU_RGB(MCUs[hauteur][largeur]);
